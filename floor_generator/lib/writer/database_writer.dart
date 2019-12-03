8055bea596a16d5189a1650857fe04371bd58ae7
zzd258147 <3059450+zzd258147@users.noreply.github.com>
2019-12-03 15:42:37 +0800

    Add support for SQLCipher
    # Conflicts:
    #	floor/pubspec.yaml
    #	floor_generator/lib/writer/database_writer.dart

--- floor_generator/lib/writer/database_writer.dart
+++ floor_generator/lib/writer/database_writer.dart
@@ -99,14 +99,19 @@ class DatabaseWriter implements Writer {
       ..name = 'callback'
       ..type = refer('Callback'));
 
+    final passwordParameter = Parameter((builder) => builder
+      ..name = 'password'
+      ..type = refer('String'));
+
     return Method((builder) => builder
       ..name = 'open'
       ..returns = refer('Future<sqflite.Database>')
       ..modifier = MethodModifier.async
       ..requiredParameters.addAll([pathParameter, migrationsParameter])
-      ..optionalParameters.add(callbackParameter)
+      ..optionalParameters.addAll([callbackParameter, passwordParameter])
       ..body = Code('''
           final databaseOptions = sqflite.OpenDatabaseOptions(
+            password: password,
             version: ${database.version},
             onConfigure: (database) async {
               await database.execute('PRAGMA foreign_keys = ON');
